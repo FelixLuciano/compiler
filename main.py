b1a7395de4e936b584831987bd4d0fcccfcf6010
FelixLuciano <felix.lucianodias@gmail.com>
2023-08-22 19:52:23 -0300

    refactor: run parser

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-import string
 import argparse
+
+from src.Parser import Parser
 
 
 def main(args: argparse.Namespace):
-    answer: int = 0
-    current = 0
-    operation = 1
-    is_empty: bool = True
-
-    if len(args.expression) < 1:
-        raise ValueError("Cannot read empty expressions!")
-
-    for index, char in enumerate(args.expression):
-        if is_empty and char in ("-", "+"):
-            raise ValueError(f"Invalid token at {index}!")
-        elif char in string.digits:
-            is_empty = False
-            current = 10 * current + int(char)
-        elif char == "+":
-            answer += current * operation
-            current = 0
-            operation = 1
-        elif char == "-":
-            answer += current * operation
-            current = 0
-            operation = -1
-        elif char in string.whitespace:
-            continue
-        else:
-            raise ValueError(f"Invalid token at {index}!")
-        
-    answer += current * operation
-        
-    print(answer)
+    print(Parser.run(args.expression))
 
 
 if __name__ == "__main__":
