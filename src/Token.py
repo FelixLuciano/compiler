589b0cd10905fb8f78dbdc3c9c73ae3758e18661
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-03 23:09:17 -0300

    feat: SPACE and DIGIT types, Static tokens and check

--- src/Token.py
+++ src/Token.py
@@ -1,4 +1,5 @@
 import string
+import typing as T
 from enum import Enum, auto
 from dataclasses import dataclass, field
 
@@ -8,19 +9,29 @@ class Token:
     type: str = field()
     value: int = field()
 
+    LAMBDA: "Token" = field(init=False, repr=False)
+    EOF: "Token" = field(init=False, repr=False)
+
     class types(Enum):
-        LAMBDA = None
-        EOF = auto()
-        NUMBER = auto()
+        LAMBDA = auto()
+        SPACE = auto()
+        DIGIT = auto()
         PLUS = auto()
         MINUS = auto()
         MULT = auto()
         DIV = auto()
+        EOF = auto()
+
+        CHAINING_TYPES: T.Tuple["Token.types"]
 
         @classmethod
         def get(cls, value: str):
+            if len(value) == 0:
+                return cls.LAMBDA
+            if value in string.whitespace:
+                return cls.SPACE
             if value in string.digits:
-                return cls.NUMBER
+                return cls.DIGIT
             elif value == "+":
                 return cls.PLUS
             elif value == "-":
@@ -29,5 +40,17 @@ class Token:
                 return cls.MULT
             elif value == "/":
                 return cls.DIV
-            
-            raise ValueError(f"\"{value}\" is not a valid token!")
+            elif value == "\0":
+                return cls.EOF
+            else:
+                return None
+
+    def check(self, type: types):
+        return self.type == type.name
+
+
+Token.LAMBDA = Token(Token.types.LAMBDA.name, None)
+Token.EOF = Token(Token.types.EOF.name, None)
+Token.types.CHAINING_TYPES = (Token.types.SPACE, Token.types.DIGIT)
+
+Token.types.CHAINING_TYPES
