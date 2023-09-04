b2034b4e94b2931cf201323b9316dfd369b652c5
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-03 23:27:34 -0300

    feat: parse expression

--- src/Parser.py
+++ src/Parser.py
@@ -11,39 +11,71 @@ class Parser:
     def parse_expression(self) -> int:
         answer = self.parse_term()
 
-        print(True, answer)
-
         while True:
-            try:
-                self.tokenizer.select_next(Token.types.PLUS)
-                answer += self.parse_term()
-            except ValueError:
-                self.tokenizer.select_next(Token.types.MINUS)
-                answer -= self.parse_term()
-            except ValueError:
-                self.tokenizer.select_next(Token.types.EOF)
+            if self.tokenizer.next.check(Token.types.PLUS):
+                operator = Token.types.PLUS
+            elif self.tokenizer.next.check(Token.types.MINUS):
+                operator = Token.types.MINUS
+            else:
                 break
-            except:
-                print("AAAAAAAaaa")
+
+            self.tokenizer.select_next()
+
+            next = self.parse_term()
+
+            if operator == Token.types.PLUS:
+                answer += next
+            elif operator == Token.types.MINUS:
+                answer -= next
 
         return answer
 
     def parse_term(self) -> int:
-        answer = self.tokenize.select_next(Token.types.NUMBER.name).value
+        answer = 0
+
+        while self.tokenizer.next == Token.LAMBDA:
+            self.tokenizer.select_next()
+
+        if self.tokenizer.next.check(Token.types.DIGIT):
+            answer = self.tokenizer.next.value
+        else:
+            raise ValueError(
+                f"Unexpected token {self.tokenizer.next.type} at {self.tokenizer.position}!"
+            )
+
+        self.tokenizer.select_next()
 
         while True:
-            try:
-                self.tokenizer.select_next(Token.types.MULT)
-                answer *= self.tokenizer.select_next(Token.types.NUMBER).value
-            except ValueError:
-                self.tokenizer.select_next(Token.types.DIV)
-                answer //= self.tokenizer.select_next(Token.types.NUMBER).value
-            except Exception:
+            if self.tokenizer.next.check(Token.types.MULT):
+                operator = Token.types.MULT
+            elif self.tokenizer.next.check(Token.types.DIV):
+                operator = Token.types.DIV
+            else:
                 break
+
+            self.tokenizer.select_next()
+
+            if self.tokenizer.next.check(Token.types.DIGIT):
+                if operator == Token.types.MULT:
+                    answer *= self.tokenizer.next.value
+                elif operator == Token.types.DIV:
+                    answer //= self.tokenizer.next.value
+            else:
+                raise ValueError(
+                    f"Unexpected token {self.tokenizer.next.type} at {self.tokenizer.position}!"
+                )
+
+            self.tokenizer.select_next()
 
         return answer
 
     @staticmethod
     def run(code: str) -> int:
-        return Parser(Tokenizer(code)).parse_expression()
+        parser = Parser(Tokenizer(code))
+        answer = parser.parse_expression()
 
+        if parser.tokenizer.next != Token.EOF:
+            print(answer, parser.tokenizer.next)
+            raise ValueError(f'Invalid expression "{code}"')
+
+        return answer
