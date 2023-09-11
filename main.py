2fe1897912a1b9f53052176c54251c2a1ccd220e
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-11 16:59:21 -0300

    feat: reads entry file and cout ast evaluation

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from src.Parser import Parser
 
 
 def main(args: argparse.Namespace):
-    print(Parser.run(args.expression))
+    with open(args.entry, "r") as entry:
+        code = entry.read()
+
+    expression = Parser.run(code)
+
+    print(expression.evaluate())
 
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
 
-    args.add_argument("expression", help='Sum expression. Eg.: "1 + 1"')
+    args.add_argument("entry", help='Program entry file. Eg.: "main.go"')
 
     main(args.parse_args())
