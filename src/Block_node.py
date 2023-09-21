1468e9b8ff25913156d4feb11b3b5dc0eea5267c
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-21 20:10:04 -0300

    feat: evaluates with context

--- src/Block_node.py
+++ src/Block_node.py
@@ -1,7 +1,9 @@
 from src.Abstract_node import Abstract_node
 
+from src.SymbolTable import SymbolTable
+
 
 class Block_node(Abstract_node):
-    def evaluate(self) -> None:
+    def evaluate(self, context: SymbolTable) -> None:
         for child in self.children:
-            child.evaluate()
+            child.evaluate(context)
