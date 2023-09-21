d42b0ccff83a134f92210506b6e79638310c42e2
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-21 20:09:53 -0300

    feat: symbol table context

---
+++ src/SymbolTable.py
@@ -0,0 +1,22 @@
+import typing as T
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SymbolTable:
+    table : T.Dict[str, int | T.Callable] = field(default_factory=dict)
+
+    RESERVED = {
+        "print": print
+    }
+
+    def set(self, identifier: str, value: int) -> None:
+        self.table[identifier] = value
+    
+    def get(self, identifier: str) -> T.Union[int, T.Callable]:
+        if identifier in self.RESERVED:
+            return self.RESERVED[identifier]
+        elif identifier in self.table:
+            return self.table[identifier]
+        else:
+            raise ValueError(f"{identifier} was not assigned.")
