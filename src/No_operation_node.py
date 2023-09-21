65742ececab628cc192868d622c4234dfdec9d31
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-21 15:40:23 -0300

    fix: proper class name

--- src/No_operation_node.py
+++ src/No_operation_node.py
@@ -1,7 +1,7 @@
 from src.Abstract_node import Abstract_node
 
 
-class Integer_value_node(Abstract_node):
+class No_operation_node(Abstract_node):
     value : None
 
     def evaluate(self) -> None:
