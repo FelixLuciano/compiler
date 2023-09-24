7daa5b7f4c8417bd45fb7f742f1822df780a66e2
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-24 18:42:01 -0300

    feat: fix class initialization

--- src/No_operation_node.py
+++ src/No_operation_node.py
@@ -1,10 +1,12 @@
-from src.Abstract_node import Abstract_node
+from dataclasses import dataclass, field
 
+from src.Abstract_node import Abstract_node
 from src.SymbolTable import SymbolTable
 
 
+@dataclass
 class No_operation_node(Abstract_node):
-    value : None
+    value: None = field(default=None)
 
-    def evaluate(self) -> None:
+    def evaluate(self, context: SymbolTable) -> None:
         return None
