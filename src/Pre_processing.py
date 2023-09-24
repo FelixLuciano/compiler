ccb0a5ecc457dc325e340016c3a9fb065ba2d219
FelixLuciano <felix.lucianodias@gmail.com>
2023-09-24 12:22:38 -0300

    fix: comment removal

--- src/Pre_processing.py
+++ src/Pre_processing.py
@@ -1,7 +1,12 @@
+import re
+
+
 class Pre_processing:
+    MATCH_COMMENTS = r"//.*(?=\n)"
+
     @staticmethod
     def filter(code: str):
         try:
-            return code[:code.index("//")]
+            return re.sub(Pre_processing.MATCH_COMMENTS, "", code, 0, re.MULTILINE)
         except ValueError:
             return code
