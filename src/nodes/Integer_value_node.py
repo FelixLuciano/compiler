c0d257e5bbdef1669fa9f9d5b9b7029182e952ef
FelixLuciano <felix.lucianodias@gmail.com>
2023-10-26 15:21:50 -0300

    feat: integer typed evaluate

--- src/nodes/Integer_value_node.py
+++ src/nodes/Integer_value_node.py
@@ -1,9 +1,15 @@
+from dataclasses import dataclass, field
+
 import src.nodes as nodes
-from src.SymbolTable import SymbolTable
+from src.SymbolTable import SymbolTable, Typed_value
 
 
+@dataclass
 class Integer_value_node(nodes.Node):
-    value: int
+    value: str = field()
 
-    def evaluate(self, context: SymbolTable) -> int:
-        return int(self.value)
+    def evaluate(self, context: SymbolTable):
+        return Typed_value(
+            type=Typed_value.types.INT,
+            value=int(self.value)
+        )
