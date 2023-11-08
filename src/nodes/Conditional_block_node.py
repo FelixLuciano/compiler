8925f0a87f72883ec776c3adb32e7ba1d6a6da75
FelixLuciano <felix.lucianodias@gmail.com>
2023-11-08 12:55:56 -0300

    feat: fix conditional jumps

--- src/nodes/Conditional_block_node.py
+++ src/nodes/Conditional_block_node.py
@@ -15,12 +15,14 @@ class Conditional_block_node(nodes.Node):
         value: Typed_value = self.children[0].evaluate(context, program)
         else_label_name = f"ELSE_{self.id}"
         endif_label_name = f"ENDIF_{self.id}"
-
+        program.write(f"JNE {else_label_name}")
         # if Typed_value.is_true(value):
         self.children[1].evaluate(context, program)
-        program.write(f"JMP {endif_label_name}")
+        program.write(
+            f"JMP {endif_label_name}",
+            f"{else_label_name}:",
+        )
         if (len(self.children) > 2):
-            program.write(f"{else_label_name}:")
             self.children[2].evaluate(context, program)
         program.write(f"{endif_label_name}:")
 
